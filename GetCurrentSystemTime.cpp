654108186aa9a203749d7aec991a5510dd1d3aca
ChaoPei <peichao0118@gmail.com>
2017-05-22 16:54:56 +0800

    获取当前系统时间

---
+++ GetCurrentSystemTime.cpp
@@ -0,0 +1,22 @@
+/**
+ * 获取当前系统时间
+    * 不受平台限制
+    * C++11实现
+ */
+
+#include <chrono>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const std::string getCurrentSystemTime()
+{
+	auto tt = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	struct tm* ptm = localtime(&tt);
+	char date[60] = { 0 };
+	sprintf(date, "%d-%02d-%02d_%02d:%02d:%02d",
+		(int)ptm->tm_year + 1900, (int)ptm->tm_mon + 1, (int)ptm->tm_mday,
+		(int)ptm->tm_hour, (int)ptm->tm_min, (int)ptm->tm_sec);
+	return std::string(date);
+}
