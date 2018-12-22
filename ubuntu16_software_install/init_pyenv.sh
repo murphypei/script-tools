c6db43a0c839d45522356989d005107d995b3af9
peichao <peichao@bigo.sg>
2018-12-22 11:16:13 +0800

    add basic vimrc

--- ubuntu16_software_install/init_pyenv.sh
+++ ubuntu16_software_install/init_pyenv.sh
@@ -5,8 +5,6 @@ set -e
 
 SHELL_DIR="$( cd "$(dirname "$0")" && pwd)"
 
-sudo apt update
-
 cd ~/Downloads
 
 wget https://bootstrap.pypa.io/get-pip.py
@@ -17,6 +15,7 @@ sudo rm -rf ./get-pip.py ~/.cache/pip
 # config virtual envs
 echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.bashrc
 echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
+echo "export VIRTUALENVWRAPPER_HOOK_DIR=$HOME/.virtualenvs" >> ~/.bashrc
 echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python" >> ~/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
 
@@ -26,8 +25,8 @@ source ~/.bashrc
 
 
 mkvirtualenv py2 -p /usr/bin/python2
-workon cv
-pip install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple -r ${SHELL_DIR}/requirements.txt 
+workon py2
+pip install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com -r ${SHELL_DIR}/requirements.txt --no-cache-dir
 
 
 
