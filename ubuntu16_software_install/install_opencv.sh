b312bc413610dcb7040491c63e496c676ce23ce4
peichao <peichao@bigo.sg>
2019-02-01 14:10:59 +0800

    complete and identify install opencv script

--- ubuntu16_software_install/install_opencv.sh
+++ ubuntu16_software_install/install_opencv.sh
@@ -1,56 +1,68 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -x
 set -e
 
-sudo apt update
+OPENCV_VERSION="3.4.1"
+OPERATION_DIR=~/tmp
+PYTHON_BIN_PATH=$(which python)
+APT_UPDATE=false
+DOWNLOAD=false
 
-# install developer tools
-sudo apt install -fy build-essential cmake unzip pkg-config
+if [ "$APT_UPDATE" = true ] ; then
+    sudo apt update
 
-# install OpenCV-specific prerequisites
-sudo apt install -fy libjpeg-dev libpng-dev libtiff-dev
+    # install developer tools
+    sudo apt install -fy build-essential cmake unzip pkg-config
 
-# install video I/O packages
-sudo apt install -fy libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
+    # install OpenCV-specific prerequisites
+    sudo apt install -fy libjpeg-dev libpng-dev libtiff-dev
 
-# install GTK for highgui module
-sudo apt install -fy libgtk-3-dev
+    # install video I/O packages
+    sudo apt install -fy libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
 
-# install optimizations
-sudo apt install -fy libatlas-base-dev gfortran
+    # install GTK for highgui module
+    sudo apt install -fy libgtk-3-dev
 
-cd ~/Downloads
+    # install optimizations
+    sudo apt install -fy libatlas-base-dev gfortran
+fi
 
-# compile and install opencv
-wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.1.zip
-wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.4.1.zip
-unzip opencv.zip
-unzip opencv_contrib.zip
+if [[ ! -d ${OPERATION_DIR} ]]; then
+    mkdir -p ${OPERATION_DIR}
+fi
+cd ${OPERATION_DIR}
 
-cd ./opencv-3.4.1/
+if [[ "$DOWNLOAD" = true ]] ; then
+    # compile and install opencv
+    wget -O opencv.zip https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip
+    wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip
+    unzip opencv.zip
+    unzip opencv_contrib.zip
+fi
+
+cd ./opencv-${OPENCV_VERSION}/
 rm -rf build
 mkdir build
 cd build
-workon cv
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
     -D INSTALL_PYTHON_EXAMPLES=ON \
     -D INSTALL_C_EXAMPLES=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=~/Downloads/opencv_contrib-3.4.1/modules \
-    -D PYTHON_EXECUTABLE=/usr/bin/python \
+    -D OPENCV_EXTRA_MODULES_PATH=${OPERATION_DIR}/opencv_contrib-${OPENCV_VERSION}/modules \
+    -D PYTHON_EXECUTABLE=${PYTHON_BIN_PATH} \
     -D BUILD_EXAMPLES=ON ..
 
 make -j4
 sudo make install
 
 # add python package in virtual env
-sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
-cd ~/.virtualenvs/cv/lib/python2.7/site-packages/
-ln -s /usr/local/lib/python2.7/site-packages/cv2.so cv2.so
-sudo ldconfig
+# sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+# cd ~/.virtualenvs/cv/lib/python2.7/site-packages/
+# ln -s /usr/local/lib/python2.7/site-packages/cv2.so cv2.so
+# sudo ldconfig
 
 # clean
-cd ~/Downloads
+cd ${OPERATION_DIR}
 rm -rf opencv.zip opencv_contrib.zip opencv-3.4.1 opencv_contrib-3.4.1
 
